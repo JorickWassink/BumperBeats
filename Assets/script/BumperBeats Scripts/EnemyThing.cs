2f1d7f8e403b61030232162f06fddf56d9bbeb58
Sam Schutten <0355622@student.rocvantwente.nl>
2024-04-26 12:57:18 +0200

    Feat: enemy bullets spawnen voor de enemy
    en de enemies hebben colission

--- Assets/script/BumperBeats Scripts/EnemyThing.cs
+++ Assets/script/BumperBeats Scripts/EnemyThing.cs
@@ -6,17 +6,20 @@ public class EnemyThing : MonoBehaviour
     [SerializeField] Transform playerPos;
     [SerializeField] Transform enemyPos;
     [SerializeField] GameObject Bullet;
+
+    Transform spawnBullet;
     public int BulletCount = 0;
     void Start()
     {
+        spawnBullet = this.gameObject.transform.GetChild(0);
         transform.up = playerPos.position - transform.position;
         StartCoroutine(SpawnBullet());
     }
 
     IEnumerator SpawnBullet()
     {
-        yield return new WaitForSeconds(Random.Range(0f,3f));
-        Instantiate(Bullet, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(0f, 3f));
+        Instantiate(Bullet, spawnBullet.position, Quaternion.identity);
         BulletCount++;
     }
 }
