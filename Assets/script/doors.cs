06090c8dda6f3b6f02799283454de02fe6fbdde6
LeonBijker <0351849@student.rocvantwente.nl>
2024-04-26 11:46:21 +0200

    up:doorscs en coins cs gemaakt

--- Assets/script/doors.cs
+++ Assets/script/doors.cs
@@ -9,6 +9,7 @@ public class doors : MonoBehaviour
     public bool getCoinsDoor;
     public bool MainGameDoor;
     public bool pinballDoor;
+    [SerializeField] Transform playertransform;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,14 @@ public class doors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && transform.position == new Vector3(8, 2.07f, 0))
         {
             getCoinsDoor = true;
+            playertransform.position = new Vector3 (6, 1.5748f, 0);
         }
         if(collision.CompareTag("Player") && transform.position == new Vector3(14.5f, 5.421f, 0))
         {
