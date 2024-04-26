9d2a7bcb37524496eedb90c4f6dba340a6ab73ff
Sam Schutten <0355622@student.rocvantwente.nl>
2024-04-26 13:12:50 +0200

    Up: Enemy Death en Bullet Deletion
    wanneer een enemy geraakt word door zn eigen bullet gaat hij weg, en wordt de bullet gedelete

--- Assets/script/BumperBeats Scripts/EnemyThing.cs
+++ Assets/script/BumperBeats Scripts/EnemyThing.cs
@@ -8,18 +8,23 @@ public class EnemyThing : MonoBehaviour
     [SerializeField] GameObject Bullet;
 
     Transform spawnBullet;
-    public int BulletCount = 0;
     void Start()
     {
         spawnBullet = this.gameObject.transform.GetChild(0);
         transform.up = playerPos.position - transform.position;
         StartCoroutine(SpawnBullet());
+
     }
 
+    IEnumerator SpawnManager()
+    {
+        yield return new WaitForSeconds(2);
+        StartCoroutine(SpawnBullet());
+    }
     IEnumerator SpawnBullet()
     {
         yield return new WaitForSeconds(Random.Range(0f, 3f));
         Instantiate(Bullet, spawnBullet.position, Quaternion.identity);
-        BulletCount++;
+        StartCoroutine(SpawnManager());
     }
 }
