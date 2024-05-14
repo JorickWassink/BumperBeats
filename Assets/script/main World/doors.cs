69b72e3163d478ac24a1eceb87b1a4d9d2ec1b40
LeonBijker <0351849@student.rocvantwente.nl>
2024-05-14 10:52:12 +0200

    misc leon2 scene

--- Assets/script/main World/doors.cs
+++ Assets/script/main World/doors.cs
@@ -6,14 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class doors : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     [SerializeField] Transform playertransform;
     [SerializeField] coins coins;
     [SerializeField] TMP_Text NoCoins;
+=======
+    public int doorIndex;
+    public bool getCoinsDoor;
+    public bool MainGameDoor;
+    public bool pinballDoor;
+>>>>>>> Stashed changes
 
     void Start()
     {
+<<<<<<< Updated upstream
         NoCoins.enabled = false;
+=======
+        doorIndex = -1;
+        getCoinsDoor = false;
+        MainGameDoor = false;
+        pinballDoor = false;
+>>>>>>> Stashed changes
     }
     
     public void Coinsdoor()
@@ -27,6 +41,7 @@ public class doors : MonoBehaviour
     }
     public void firstGameDoor()
     {
+<<<<<<< Updated upstream
 
         if(coins.CoinsCount < 1) 
         {
@@ -42,13 +57,40 @@ public class doors : MonoBehaviour
             SceneManager.LoadScene("sam");
          
         }
+=======
+>>>>>>> Stashed changes
     }
     IEnumerator wait(float seconds)
     {
+<<<<<<< Updated upstream
         yield return new WaitForSeconds(seconds);
     }
     public void ReturnFirstGameDoor()
     {
         SceneManager.LoadScene("leon");
+=======
+        if (collision.CompareTag("Player") && transform.position == new Vector3(8, 2.07f, 0))
+        {
+            doorIndex = 0;
+            getCoinsDoor = true;
+        }
+        if (collision.CompareTag("Player") && transform.position == new Vector3(14.5f, 5.421f, 0))
+        { doorIndex = 1;
+            MainGameDoor = true;
+        }
+        if(collision.CompareTag("Player") && transform.position == new Vector3(23.83f, 2.17f, 0))
+        {
+            doorIndex = 2;
+            pinballDoor = true;
+        }
+        if (collision.CompareTag("Player") && getCoinsDoor == true)
+        {
+            SceneManager.LoadScene("Jorick");
+        }
+        if (collision.CompareTag("Player") && MainGameDoor == true)
+        {
+            SceneManager.LoadScene("Sam");
+        }
+>>>>>>> Stashed changes
     }
 }
