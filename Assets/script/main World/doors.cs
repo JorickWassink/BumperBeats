c678d017d6940b6540adac21d087555bd4902edd
LeonBijker <0351849@student.rocvantwente.nl>
2024-05-14 09:22:34 +0200

    feat deur

--- Assets/script/main World/doors.cs
+++ Assets/script/main World/doors.cs
@@ -5,20 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class doors : MonoBehaviour
 {
-    public int doorIndex;
-    public bool getCoinsDoor;
-    public bool MainGameDoor;
-    public bool pinballDoor;
+
     [SerializeField] Transform playertransform;
     [SerializeField] coins coins;
-
     // Start is called before the first frame update
     void Start()
     {
-        doorIndex = -1;
-        getCoinsDoor = false;
-        MainGameDoor = false;
-        pinballDoor = false;
+     
         
     }
 
@@ -29,23 +22,22 @@ public class doors : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && transform.position == new Vector3(8, 2.07f, 0))
-        {
-            getCoinsDoor = true;
-            playertransform.position = new Vector3 (6, 1.5748f, 0);
-        }
-        if(collision.CompareTag("Player") && transform.position == new Vector3(14.5f, 5.421f, 0))
-        {
-            MainGameDoor = true;
-        }
-        if (collision.CompareTag("Player") && getCoinsDoor == true)
+        
+        if (collision.CompareTag("Player") && playertransform.position == new Vector3(7.914441f, 2.11f, 0))// deze is voor coins
         {
             SceneManager.LoadScene("Jorick");
         }
-        if (collision.CompareTag("Player") && MainGameDoor == true)
+        if(collision.CompareTag("Player") && playertransform.position == new Vector3(14.5f, 5.421f, 0))// dit is voor eerste game
         {
             coins.removecoin();
             SceneManager.LoadScene("Sam");
         }
+
+
+    }
+    public void ReturnCoinsDoor()
+    {
+        SceneManager.LoadScene("Leon");
+        playertransform.position = new Vector3(10, 1.5748f, 0);
     }
 }
