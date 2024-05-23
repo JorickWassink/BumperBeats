1b8a415dc041a8dcfee1d7f898b1a975be3d173a
Sam Schutten <0355622@student.rocvantwente.nl>
2024-05-23 09:36:46 +0200

    Revert "Merge branch 'main' into Feat-Sam"
    This reverts commit 46ebf662a2337843c4c5afa56a0feeeaf1d120a3, reversing
    changes made to 8bfbc240cbcbaeeb766e174183e58028d7a5d1ce.

--- Assets/script/main World/coins.cs
+++ Assets/script/main World/coins.cs
@@ -1,7 +1,8 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class coins : MonoBehaviour
 {
