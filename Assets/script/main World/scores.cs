1190c578fd99b87076d639c9fbc5322046e8c0ec
LeonBijker <0351849@student.rocvantwente.nl>
2024-05-23 13:13:21 +0200

    misc

--- Assets/script/main World/scores.cs
+++ Assets/script/main World/scores.cs
@@ -8,16 +8,22 @@ public class scores : MonoBehaviour
 {
     public TMP_Text plincohighscore;
     int Plincoscore;
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
     }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-         Plincoscore = PlayerPrefs.GetInt("plincoscore", 0);
+        Plincoscore = PlayerPrefs.GetInt("plincoscore");
         if (plincohighscore != null)// checkt of plincohihgscore niet leeg is
         {
             plincohighscore.text = Plincoscore.ToString();// zet plincoscore naar een string en zet dat op de text van plincohihgscore
