59e0bf691598d2d471f72939af854a03e74a08f8
LeonBijker <0351849@student.rocvantwente.nl>
2024-05-24 09:52:10 +0200

    fix highscore voor plincogame

--- Assets/script/main World/scores.cs
+++ Assets/script/main World/scores.cs
@@ -6,27 +6,27 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class scores : MonoBehaviour
 {
-    public TMP_Text plincohighscore;
+    public TMP_Text plincotext;
     int Plincoscore;
-    private void Awake()
-    {
-        
-    }
+    public static bool firstload;
     // Start is called before the first frame update
     void Start()
     {
-       
-        
+        if (firstload == false)
+        {
+            PlayerPrefs.SetInt("plinco", 0);
+            firstload = true;
+        }
+        if (plincotext != null)// checkt of plincohighscore niet leeg is
+        {
+            plincotext.text = PlayerPrefs.GetInt("plinco").ToString();// zet plincoscore naar een string en zet dat op de text van plincohihgscore
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Plincoscore = PlayerPrefs.GetInt("plincoscore");
-        if (plincohighscore != null)// checkt of plincohihgscore niet leeg is
-        {
-            plincohighscore.text = Plincoscore.ToString();// zet plincoscore naar een string en zet dat op de text van plincohihgscore
-        }
+
     }
 }
