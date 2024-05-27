d9781aa284356d8cc675aaa09d4d861e7b0dd23c
Jorick Wassink <wassinkjorick@gmail.com>
2024-05-27 12:23:40 +0100

    feat: flippers werken
    en ook mischien bug gefixt waar bal in de flipper ging

--- Assets/script/Pinball/PinBall.cs
+++ Assets/script/Pinball/PinBall.cs
@@ -1,37 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PinBall : MonoBehaviour
 {
     private Rigidbody2D rb;
     private int force = 5;
+    flippers flip;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        flip = FindAnyObjectByType<flippers>(); 
         // save kant op waar die heen gaat
         // push steeds mee naar die kant met velocity
         // die omhoog gaat als je een bumper aanraakt
     }
 
-    void LateUpdate()
-    {        
-        Vector3 wDirection = rb.velocity.normalized;
-        Vector3 lDirection = Vector3.right;
-
-        Vector3 currentWDirection = transform.TransformDirection(lDirection);
-
-        float angleDiff = Vector3.SignedAngle(currentWDirection,wDirection, Vector3.forward);
-
-        transform.Rotate(Vector3.forward, angleDiff, Space.World);
+    private void Update()
+    {
+        //print(flip.holdingSpace);
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.tag.Contains("Bumper"))
-    //    {
-    //        rb.AddForce(transform.forward * 5);
-    //    }        
-    //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Player")) if (flip.holdingSpace) rb.AddForce(transform.up * 1000);
+    }
 }
