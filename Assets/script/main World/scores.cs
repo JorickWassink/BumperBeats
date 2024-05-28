82a636f42df3e0f0f7e538df886710b79f19a264
LeonBijker <0351849@student.rocvantwente.nl>
2024-05-28 09:44:00 +0200

    feat added highscore text
    added highscore text to all games in the hub world and added it so that the highscore for pinbal game works

--- Assets/script/main World/scores.cs
+++ Assets/script/main World/scores.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class scores : MonoBehaviour
 {
     public TMP_Text plincotext;
-    int Plincoscore;
+    public TMP_Text PinBallText;
     public static bool firstload;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +15,16 @@ public class scores : MonoBehaviour
         if (firstload == false)
         {
             PlayerPrefs.SetInt("plinco", 0);
+            PlayerPrefs.SetInt("PinBallScore", 0);
             firstload = true;
         }
         if (plincotext != null)// checkt of plincohighscore niet leeg is
         {
             plincotext.text = PlayerPrefs.GetInt("plinco").ToString();// zet plincoscore naar een string en zet dat op de text van plincohihgscore
+        }
+        if(PinBallText != null)
+        {
+            PinBallText.text = PlayerPrefs.GetInt("PinBallScore").ToString();
         }
     }
 
